9aedb3d7d8fa98e746825d2de07d802e4cce686c
techtwins <gshevaughn@gmail.com>
2020-12-20 11:13:52 -0500

    done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
+  let shows = movies.map((movie, id) => (
+    <div key={id}>
+      <h3>Title: </h3> {movie.title}
+      <br />
+      <h4>Time: </h4> <span>{movie.time} mins </span>
+      <h3>Genre: </h3>
+      <ul>
+        {movie.genres.map((genre, id) => (
+          <li key={id}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {shows}
     </div>
   );
 };
